37cbaca406f38d9344d939c84bf290711a21bf77
Brian Bissette <bbissette3@yahoo.com>
2023-02-01 16:27:16 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+ 
+
+function writeCards(cards, event) {
+    const newArray = []
+    for (let i = 0; i < cards.length; i++) {
+        newArray.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+    return newArray;
+}
+
+function countDown(number) {
+        while (number >= 0){
+        console.log(number--);
+        }
+}
